9e0ad10b4d67a153ab4dc7c26cab354644e8b32f
Maxime Coquelin <maxime.coquelin@redhat.com>
2023-02-07 17:22:39 +0100

    vhost: fix possible FD leaks
    [ upstream commit 585283f9a703fcd994dbf9aca07dea9445319862 ]
    
    On failure, read_vhost_message() only closed the message
    FDs if the header size was unexpected, but there are other
    cases where it is required. For example in the case the
    payload size read from the header is greater than the
    expected maximum payload size.
    
    This patch fixes this by closing all messages FDs in all
    error cases.
    
    Fixes: bf472259dde6 ("vhost: fix possible denial of service by leaking FDs")
    
    Signed-off-by: Maxime Coquelin <maxime.coquelin@redhat.com>
    Signed-off-by: David Marchand <david.marchand@redhat.com>
    Reviewed-by: Chenbo Xia <chenbo.xia@intel.com>

--- lib/vhost/vhost_user.c
+++ lib/vhost/vhost_user.c
@@ -2693,28 +2693,35 @@ read_vhost_message(struct virtio_net *dev, int sockfd, struct  vhu_msg_context *
 
 	ret = read_fd_message(dev->ifname, sockfd, (char *)&ctx->msg, VHOST_USER_HDR_SIZE,
 		ctx->fds, VHOST_MEMORY_MAX_NREGIONS, &ctx->fd_num);
-	if (ret <= 0) {
-		return ret;
-	} else if (ret != VHOST_USER_HDR_SIZE) {
+	if (ret <= 0)
+		goto out;
+
+	if (ret != VHOST_USER_HDR_SIZE) {
 		VHOST_LOG_CONFIG(ERR, "(%s) Unexpected header size read\n", dev->ifname);
-		close_msg_fds(ctx);
-		return -1;
+		ret = -1;
+		goto out;
 	}
 
 	if (ctx->msg.size) {
 		if (ctx->msg.size > sizeof(ctx->msg.payload)) {
 			VHOST_LOG_CONFIG(ERR, "(%s) invalid msg size: %d\n",
 					dev->ifname, ctx->msg.size);
-			return -1;
+			ret = -1;
+			goto out;
 		}
 		ret = read(sockfd, &ctx->msg.payload, ctx->msg.size);
 		if (ret <= 0)
-			return ret;
+			goto out;
 		if (ret != (int)ctx->msg.size) {
 			VHOST_LOG_CONFIG(ERR, "(%s) read control message failed\n", dev->ifname);
-			return -1;
+			ret = -1;
+			goto out;
 		}
 	}
+
+out:
+	if (ret <= 0)
+		close_msg_fds(ctx);
 
 	return ret;
 }
